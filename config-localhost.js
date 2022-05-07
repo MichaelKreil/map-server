9bde2b088a2025d0940cab8bfcb3751475443b38
Michael Kreil <github@michael-kreil.de>
2022-05-07 18:52:28 +0200

    don't commit config.js

---
+++ config-localhost.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+	dbName: 'germany',
+	port: 8080,
+	baseUrl: 'http://localhost:8080',
+	corsHeader: { 'Access-Control-Allow-Origin': '*' },
+	useCaching: true,
+	useSSL: false,
+}
