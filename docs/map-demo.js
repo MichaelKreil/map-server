8919d8d4ee30a0f8aab479fb0fce254850be9a1d
Michael Kreil <github@michael-kreil.de>
2022-07-22 21:23:42 +0200

    cleanup

--- docs/map-demo.js
+++
@@ -1,8 +0,0 @@
-
-//<link href='https://karten.party/static/maplibre-gl-1.15.3/maplibre-gl.css' rel='stylesheet' />
-//<script src='/static/maplibre-gl-1.15.3/maplibre-gl.js'></script>
-import * as map from 'https://karten.party/static/maplibre-gl-1.15.3/maplibre-gl.js'
-
-console.log('demo');
-
-
