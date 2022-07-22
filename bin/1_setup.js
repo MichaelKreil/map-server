007b12119a003ba0afb97077c83a328058a47cf2
Michael Kreil <github@michael-kreil.de>
2022-07-22 19:54:35 +0200

    add style

--- bin/1_setup.js
+++ bin/1_setup.js
@@ -1,31 +1,32 @@
 'use strict'
 
+const { } = require('../lib/helper.js');
 const tileSources = require('../lib/tilesources.js');
 
 start();
 
 async function start() {
-	console.log(`setup sources`);
+	console.log(`setup sources`.white0);
 	for (let source of tileSources) {
-		console.log(`  check source "${source.name}"`);
+		console.log(`check source "${source.name}"`.white1);
 
 		if (await source.isOkay()) {
-			console.log(`    source is ok ✅`);
+			console.log(`source is ok ✓`.green2);
 			continue
 		}
 
-		console.log(`    download "${source.name}"`);
+		console.log(`download "${source.name}"`.white2);
 
 		if (!await source.isSpaceAvailable()) {
-			console.log(`    not enough space ❗️`);
+			console.log(`not enough space !`.red2);
 			continue;
 		}
 		
 		await source.download(v => {
 			v = Math.round(v * 100);
-			process.stdout.write(`\r      ${'■'.repeat(v)}${'□'.repeat(100 - v) }  `)
+			process.stdout.write(`\r${'■'.repeat(v)}${'□'.repeat(100 - v)}   `.white2)
 		});
 
-		console.log(`\n    finished ✅`);
+		console.log(`\n    finished ✓`.green2);
 	}
 }
